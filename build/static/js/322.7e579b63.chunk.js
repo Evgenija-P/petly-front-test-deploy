a7bb61efba48b5ca1d6a6623b6014169dcc958ee
Evgenija-P <e.a.poduzova@gmail.com>
2023-02-12 23:08:44 +0200

    v5

--- build/static/js/322.7e579b63.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpetly_front_test_deploy=self.webpackChunkpetly_front_test_deploy||[]).push([[322],{1322:function(e,t,n){n.r(t);var l=n(4270),s=n(184);t.default=function(){return(0,s.jsx)("div",{children:(0,s.jsx)(l.q,{children:(0,s.jsx)("title",{children:"NewsPage"})})})}}}]);
-//# sourceMappingURL=322.7e579b63.chunk.js.map
--- build/static/js/75.9bfb2007.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpetly_front_test_deploy=self.webpackChunkpetly_front_test_deploy||[]).push([[75],{3075:function(e,t,n){n.r(t);var l=n(4270),r=n(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)(l.q,{children:(0,r.jsx)("title",{children:"UserPage"})})})}}}]);
-//# sourceMappingURL=75.9bfb2007.chunk.js.map
